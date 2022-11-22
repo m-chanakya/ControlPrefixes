bc34414e52203ed25bb4707649732c06d504af5e
Chanakya Malireddy <chanakya.malireddy@uber.com>
2022-11-21 18:18:56 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ class CustomInstall(install):
 
     def run(self):
         install.run(self)
-        os.system('pip3 install -r requirements.txt --ignore-installed')
+        # os.system('pip3 install -r requirements.txt --ignore-installed')
         os.system('pip3 uninstall transformers -y')
 
         os.system(
-            "pip install git+https://github.com/jordiclive/transformers.git@controlprefixes --ignore-installed"
+            "pip install git+https://github.com/m-chanakya/transformers.git@controlprefixes --ignore-installed"
         )
-        os.system('pip3 install torchtext==0.8.0 torch==1.7.1')
+        os.system('pip3 install torchtext==0.8.1 torch==1.7.1')
 
 
 # in the setup function:
@@ -27,6 +27,29 @@ setup(
     name="ControlPrefixes",
     version="0.1.0",
     python_requires='>=3.7',
+    install_requires=[
+        "bert-score==0.3.5",
+        "configargparse",
+        "dataclasses",
+        "datasets==1.1.2",
+        "h5py==2.10.0",
+        "fire",
+        "ipython==7.16.1",
+        "ipykernel==5.3.4",
+        "pandas==1.1.3",
+        "pyarrow==1.0.1",
+        "pytorch-lightning==1.2.10",
+        "sentencepiece==0.1.91",
+        "tensorboard",
+        "tensorflow==2.5.0",
+        "tokenizers>=0.11.1,!=0.11.3,<0.14",
+        "torchvision==0.9.0",
+        "typing==3.7.4.3",
+        "gitpython",
+        "rouge_score",
+        "sacrebleu",
+        "wandb"
+    ],
     description="Code for Control Prefixes for Parameter-Efficient Text Generation",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -37,3 +60,4 @@ setup(
     license="MIT License",
     cmdclass={'install': CustomInstall},
 )
+
